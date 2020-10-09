e0ca5691112f48f9d916d2da1ed332724ae0deb8
Orestis <40428588@live.napier.ac.uk>
2020-10-09 15:47:06 +0100

    add Steps

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace GitHubSetUp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Setup process");
+            Console.WriteLine("STEP 1:");
+            Console.WriteLine("VS menu[Tools] -> Options -> Source Control ->" +
+                " (select Git)");
+            Console.WriteLine("STEP 2:");
+            Console.WriteLine("right-click solution[Add Solution Control]" +
+                " -> Change tab to Team Explorer -> SYNC");
+            Console.ReadLine();
         }
     }
 }
